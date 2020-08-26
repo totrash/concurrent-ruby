3b4acae69b4f0de4ec83c538e733f3f237480330
Kamil <totrash@o2.pl>
2020-08-26 11:58:19 +0200

    Separate operations
    Separte read, write operations,
    Add Results

--- spec/concurrent/performance.rb
+++ spec/concurrent/performance.rb
@@ -3,17 +3,23 @@ require "benchmark/ips"
 require "concurrent"
 
 module Concurrent
-  RSpec.describe Map do
-    context "concurrency" do
-      let(:hsh) { Concurrent::Hash.new }
-      let(:map) { Concurrent::Map.new }
+  ITERATIONS = 10000
 
-      it "should be faster in conncurent env" do
+  RSpec.describe "Map|Performance" do
+    let(:hsh) { Concurrent::Hash.new }
+    let(:map) { Concurrent::Map.new }
+
+    context "In conncurent env" do
+      it "should be faster" do
+        puts "-------"
+        puts "WRITING/READING/DELETING [CONN]"
+        puts "-------"
+
         Benchmark.ips do |bm|
           bm.report "Hash" do
             (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
               in_thread do
-                1000.times do |j|
+                ITERATIONS.times do |j|
                   key = i * 1000 + j
                   hsh[key] = i
                   hsh[key]
@@ -21,12 +27,11 @@ module Concurrent
                 end
               end
             end.map(&:join)
-            expect(hsh).to be_empty
           end
           bm.report "Map" do
             (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
               in_thread do
-                1000.times do |j|
+                ITERATIONS.times do |j|
                   key = i * 1000 + j
                   map[key] = i
                   map[key]
@@ -38,32 +43,163 @@ module Concurrent
           bm.compare!
         end
       end
-    end
 
-    context "without concurrency" do
-      let(:hsh) { Concurrent::Hash.new }
-      let(:map) { Concurrent::Map.new }
+      it "should be faster" do
+        puts "-------------------"
+        puts "READING [CONN]"
+        puts "-------------------"
 
-      it "should be faster without conncurency" do
+        (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+          ITERATIONS.times do |j|
+            key = i * 1000 + j
+            hsh[key] = i
+            map[key] = i
+          end
+        end
+
         Benchmark.ips do |bm|
           bm.report "Hash" do
             (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
-              1000.times do |j|
+              in_thread do
+                ITERATIONS.times do |j|
+                  key = i * 1000 + j
+                  hsh[key]
+                end
+              end
+            end.map(&:join)
+          end
+
+          bm.report "Map" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              in_thread do
+                ITERATIONS.times do |j|
+                  key = i * 1000 + j
+                  map[key]
+                end
+              end
+            end.map(&:join)
+          end
+          bm.compare!
+        end
+      end
+
+      it "should be faster" do
+        puts "-------------------"
+        puts "WRITING [CONN]"
+        puts "-------------------"
+
+        Benchmark.ips do |bm|
+          bm.report "Hash" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              in_thread do
+                ITERATIONS.times do |j|
+                  key = i * 1000 + j
+                  hsh[key] = i
+                end
+              end
+            end.map(&:join)
+          end
+
+          bm.report "Map" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              in_thread do
+                ITERATIONS.times do |j|
+                  key = i * 1000 + j
+                  map[key] = i
+                end
+              end
+            end.map(&:join)
+          end
+          bm.compare!
+        end
+      end
+    end
+
+    context "Without conncurent env" do
+      it "should be faster" do
+        puts "-------------------"
+        puts "WRITING/READING/DELETING"
+        puts "-------------------"
+
+        Benchmark.ips do |bm|
+          bm.report "Hash" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              ITERATIONS.times do |j|
                 key = i * 1000 + j
                 hsh[key] = i
                 hsh[key]
                 hsh.delete(key)
               end
             end
-            expect(hsh).to be_empty
           end
           bm.report "Map" do
             (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
-              1000.times do |j|
+              ITERATIONS.times do |j|
                 key = i * 1000 + j
                 map[key] = i
                 map[key]
                 map.delete(key)
+              end
+            end
+          end
+          bm.compare!
+        end
+      end
+
+      it "should be faster" do
+        puts "-------------------"
+        puts "READING"
+        puts "-------------------"
+
+        (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+          ITERATIONS.times do |j|
+            key = i * 1000 + j
+            hsh[key] = i
+            map[key] = i
+          end
+        end
+        Benchmark.ips do |bm|
+          bm.report "Hash" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              ITERATIONS.times do |j|
+                key = i * 1000 + j
+                hsh[key]
+              end
+            end
+          end
+
+          bm.report "Map" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              ITERATIONS.times do |j|
+                key = i * 1000 + j
+                map[key]
+              end
+            end
+          end
+          bm.compare!
+        end
+      end
+
+      it "should be faster" do
+        puts "-------------------"
+        puts "WRITING"
+        puts "-------------------"
+
+        Benchmark.ips do |bm|
+          bm.report "Hash" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              ITERATIONS.times do |j|
+                key = i * 1000 + j
+                hsh[key] = i
+              end
+            end
+          end
+
+          bm.report "Map" do
+            (1..Concurrent::ThreadSafe::Test::THREADS).map do |i|
+              ITERATIONS.times do |j|
+                key = i * 1000 + j
+                map[key] = i
               end
             end
           end
